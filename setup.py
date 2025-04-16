67876f3cb3df7ca3524b85d1011d2253d8dc2a29
Peter Pak <ppak10@gmail.com>
2025-04-16 12:39:49 -0400

    Updates requirement files and fixes CLI entrypoint.

--- setup.py
+++ setup.py
@@ -6,9 +6,15 @@ setup(
     version="0.0.1",
     # Loads in local packages
     packages=find_packages(),
+    include_package_data=True,
     package_data={
         "am": [
             "data/**/*.ini",
         ]
     },
+    entry_points={
+        "console_scripts": [
+            "am=am.manage:main",
+        ]
+    }
 )
