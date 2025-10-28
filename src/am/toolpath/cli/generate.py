d1f66ec82eb9380e94cfdf049e0ecf789a9612ea
Peter Pak <ppak10@gmail.com>
2025-10-28 11:20:49 -0700

    Updates toolpath generation to utilize build parameters configuration.

--- src/am/toolpath/cli/generate.py
+++ src/am/toolpath/cli/generate.py
@@ -4,19 +4,32 @@ from typing_extensions import Annotated
 
 from wa.cli.options import WorkspaceOption
 
+
+
 def register_toolpath_generate(app: typer.Typer):
+    from am.schema.build_parameters import DEFAULT
+
     @app.command(name="generate")
     def toolpath_generate(
         filename: str,
-        nonplanar: Annotated[bool, typer.Option("--nonplanar")] = False,
+
+        # TODO: Move to its own CLI method of generate_nonplanar
+        # nonplanar: Annotated[bool, typer.Option("--nonplanar")] = False,
+
+        build_parameters_filename: Annotated[
+            str, typer.Option("--build-parameters", help="Build Parameters filename")
+        ] = "default.json",
+
         workspace: WorkspaceOption = None,
     ) -> None:
         """
-        Generates toolpath from loaded mesh.
+        Generates toolpath from loaded mesh (planar).
         """
         from rich import print as rprint
 
+        from am.schema import BuildParameters
         from am.toolpath import ToolpathSlicerPlanar
+
         from wa.cli.utils import get_workspace_path
 
         workspace_path = get_workspace_path(workspace)
@@ -24,11 +37,13 @@ def register_toolpath_generate(app: typer.Typer):
         try:
             filepath = workspace_path / "parts" / filename
 
-            if not nonplanar:
-                toolpath_slicer_planar = ToolpathSlicerPlanar()
-                toolpath_slicer_planar.load_mesh(filepath)
-            else:
-                raise NotImplemented 
+            build_parameters = BuildParameters.load(
+                workspace_path / "build_parameters" / build_parameters_filename
+            )
+
+            toolpath_slicer_planar = ToolpathSlicerPlanar()
+            toolpath_slicer_planar.load_mesh(filepath)
+            toolpath_slicer_planar.slice(build_parameters, workspace_path)
 
         except Exception as e:
             rprint(f"⚠️ [yellow]Unable to slice provided file: {e}[/yellow]")
--- src/am/toolpath/slicer_planar.py
+++ src/am/toolpath/slicer_planar.py
@@ -1,6 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import trimesh
 
+from datetime import datetime
+from enum import Enum
+from matplotlib.axes import Axes
+from numpy.typing import ArrayLike
 from pathlib import Path
+from pint import Quantity
+from typing import cast
+
+from am.schema import BuildParameters
+
+class ToolpathOutputFolder(str, Enum):
+    toolpaths = "toolpaths"
 
 class ToolpathSlicerPlanar:
     """
@@ -10,6 +23,53 @@ class ToolpathSlicerPlanar:
     def __init__(self):
         self.mesh: trimesh.Trimesh | None = None
 
+    def slice(
+        self,
+        build_parameters: BuildParameters,
+        workspace_path: Path,
+        run_name: str | None = None,
+    ):
+
+        # Creates run folder inside workspace for slicer runs.
+        if run_name is None:
+            run_name = datetime.now().strftime("slicer_planar_%Y%m%d_%H%M%S")
+
+        toolpaths_out_path = workspace_path / "toolpaths" / run_name
+        toolpaths_out_path.mkdir(exist_ok=True, parents=True)
+
+        # Save solver configs
+        build_parameters.save(toolpaths_out_path / "config" / "build_parameters.json")
+
+        if self.mesh is None:
+            return None
+
+        step = cast(Quantity, build_parameters.layer_height).to("mm")
+
+        z_extents = self.mesh.bounds[:, 2]
+
+        z_levels = np.arange(*z_extents, step=step.magnitude)
+
+        sections = self.mesh.section_multiplane(
+            plane_origin=self.mesh.bounds[0],
+            plane_normal=[0, 0, 1],
+            heights=z_levels
+        )
+        sections = cast(ArrayLike, sections)
+
+        slices_out_path = toolpaths_out_path / "slices"
+        slices_out_path.mkdir(exist_ok=True, parents=True)
+
+        zfill = len(f"{len(sections)}")
+
+        for section_index, section in enumerate(sections):
+            axis = section.plot_discrete()
+            axis = cast(Axes, axis)
+            segment_index_string = f"{section_index}".zfill(zfill)
+            slice_file = f"{segment_index_string}.png"
+            slice_out_path = slices_out_path / slice_file
+            plt.savefig(slice_out_path)
+            plt.close()
+
     def load_mesh(
         self,
         file_obj: Path,
@@ -17,5 +77,4 @@ class ToolpathSlicerPlanar:
         **kwargs
     ):
         self.mesh = trimesh.load_mesh(file_obj, file_type, kwargs)
-        print(f"self.mesh: {self.mesh}")
 
